05eca99943fb89f90fe3d2e2a3759c816d9821b4
Waqas Nawab <imwaqas1993@gmail.com>
2023-07-31 15:58:16 +0500

    setup postgres with type nodejs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+
+import 'reflect-metadata'
 import express from 'express'
+import { DataSource } from 'typeorm'
 
 const app = express()
 
@@ -6,10 +9,21 @@ app.use(express.json())
 
 const Port = 8000
 
+const AppDataSource = new DataSource({
+    type:'postgres',
+    host:'localhost',
+    port:5432, // should be in .env file
+    username:'postgres', // should be in .env file
+    password: 'postgres', // should be in .env file
+    database:'typeorm_db', // should be in .env file
+})
+
+AppDataSource.initialize().then(()=>{
+    app.listen(Port,()=>{
+        console.log("Running")
+    })
+}).catch(err=>{console.log(err)})
 app.get('/', function(req,res) {
     res.send("Hello")
 })
 
-app.listen(Port,()=>{
-    console.log("Running:")
-})
