012119efe5aa4d626ec102c43cb64894930acf2e
Waqas Nawab <imwaqas1993@gmail.com>
2023-08-01 12:46:22 +0500

    CRUD using typeorm postgres

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,54 @@
 import 'reflect-metadata'
 import express from 'express'
 import { DataSource } from 'typeorm'
+import { User } from './entities/User'
 
 const app = express()
 
 app.use(express.json())
 
 const PORT = 8000
+
+app.get('/', function(req,res) {
+    res.send("Service Up!")
+})
+
+app.get('/users', async function(req,res) {
+    const users = await AppDataSource.getRepository(User).find()
+    res.status(200).json(users)
+
+})
+
+app.get('/user-by-id', async function(req,res) {
+    const filteredUser = await AppDataSource.getRepository(User).findOne({where:{id:1}})
+    res.status(200).json(filteredUser)
+
+})
+
+app.get('/create-user', async function(req,res) {
+    const user : User = new User();
+    user.email = "abc@gmail.com"
+    user.firstName = "abc"
+    user.lastName = "xyz"
+    const addedUser = await AppDataSource.getRepository(User).save(user)
+    res.status(200).json(addedUser)
+
+})
+
+app.get('/update-user', async function(req,res) {
+    const user : User = new User();
+    user.firstName = "abc_updated"
+    user.lastName = "xyz"
+    const updatedUser = await AppDataSource.getRepository(User).update(1,user)
+    res.status(200).json(updatedUser)
+
+})
+
+app.get('/delete-user', async function(req,res) {
+    const deletedStatus = await AppDataSource.getRepository(User).delete(2)
+    res.status(200).json(deletedStatus)
+
+})
 
 const AppDataSource = new DataSource({
     type:'postgres',
@@ -28,7 +70,4 @@ AppDataSource.initialize().then(()=>{
 }).catch(err=>{console.log(err)})
 
 
-app.get('/', function(req,res) {
-    res.send("Hello")
-})
 
